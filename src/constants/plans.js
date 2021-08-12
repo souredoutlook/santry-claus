532bb1df5f22dd004508eeb8016abfc60703f7b8
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-08-12 15:18:10 -0400

    add OnDemand costs for both event categories

--- src/constants/plans.js
+++ src/constants/plans.js
@@ -52,6 +52,60 @@ const transactionQuotas = [
   },
 ];
 
+const transactionOnDemand = [
+  // actual quotas are 1000 * min/max
+  // costs are per transaction in USD
+  {
+    bucketId: "t1",
+    minQuota: 100,
+    maxQuota: 250,
+    businessCost: 0.000390,
+    teamCost: 0.000130,
+  },
+  {
+    bucketId: "t2",
+    minQuota: 250,
+    maxQuota: 500,
+    businessCost: 0.000231,
+    teamCost: 0.000075,
+  },
+  {
+    bucketId: "t3",
+    minQuota: 500,
+    maxQuota: 4500,
+    businessCost: 0.000169,
+    teamCost: 0.000068,
+  },
+  {
+    bucketId: "t4",
+    minQuota: 4500,
+    maxQuota: 5000,
+    businessCost: 0.000120,
+    teamCost: 0.000060,
+  },
+  {
+    bucketId: "t5",
+    minQuota: 5000,
+    maxQuota: 10000,
+    businessCost: 0.000120,
+    teamCost: 0.000060,
+  },
+  {
+    bucketId: "t6",
+    minQuota: 10000,
+    maxQuota: 20000,
+    businessCost: 0.000100,
+    teamCost: 0.000057,
+  },
+  {
+    bucketId: "t7",
+    minQuota: 20000,
+    maxQuota: 50000,
+    businessCost: 0.000096,
+    teamCost: 0.000055,
+  },
+];
+
 const errorQuotas = [
   // actual quotas are 1000 * min/max
   // costs are per event in USD 
@@ -99,5 +153,51 @@ const errorQuotas = [
   },
 ];
 
-export default transactionQuotas;
-export default errorQuotas;
+const errorOnDemand = [
+  // actual quotas are 1000 * min/max
+  // costs are per event in USD 
+  {
+    bucketId: "e1",
+    minQuota: 50,
+    maxQuota: 100,
+    businessCost: 0.001157,
+    teamCost: 0.000377,
+  },
+  {
+    bucketId: "e2",
+    minQuota: 100,
+    maxQuota: 500,
+    businessCost: 0.000650,
+    teamCost: 0.000228,
+  },
+  {
+    bucketId: "e3",
+    minQuota: 500,
+    maxQuota: 2000,
+    businessCost: 0.000390,
+    teamCost: 0.000195,
+  },
+  {
+    bucketId: "e4",
+    minQuota: 2000,
+    maxQuota: 10000,
+    businessCost: 0.000390,
+    teamCost: 0.000195,
+  },
+  {
+    bucketId: "e5",
+    minQuota: 10000,
+    maxQuota: 20000,
+    businessCost: 0.000187,
+    teamCost: 0.000156,
+  },
+  {
+    bucketId: "e6",
+    minQuota: 20000,
+    maxQuota: 50000,
+    businessCost: 0.000172,
+    teamCost: 0.000143,
+  },
+];
+
+module.exports = { transactionQuotas, transactionOnDemand, errorQuotas, errorOnDemand };
