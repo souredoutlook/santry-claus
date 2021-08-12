05f01bd68a45e1c6b63255f64229c2502f39bf7b
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-08-11 21:04:28 -0400

    add Input components to gifted events card

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import RadioList from "./components/RadioList";
 import Input from "./components/Input";
 
 import { RESERVED_EVENTS, PLAN_OPTIONS } from "./constants/ORG_DETAILS";
+import { EVENT_CONSTANTS } from "./constants/EVENT_CONSTANTS";
 import './App.css';
 
 export default function App() {
 
-  const [orgDetails, setOrgDetails] = useState({
+  const [giftDetails, setGiftDetails] = useState({
     plan: "Team",
     cycle: "Monthly",
     reservedErrors: 50,
     acceptedErrors: 0,
     reservedTransactions: 100,
     acceptedTransactions: 0,
+    giftedErrors: 0,
+    giftedTransactions: 0,
   });
 
   return (
@@ -24,37 +27,46 @@ export default function App() {
       <div className="card--org-details">
         <RadioList
           type={PLAN_OPTIONS.plan}
-          orgDetails={orgDetails}
-          setOrgDetails={setOrgDetails}
+          giftDetails={giftDetails}
+          setGiftDetails={setGiftDetails}
         />
         <RadioList 
           type={PLAN_OPTIONS.cycle}
-          orgDetails={orgDetails}
-          setOrgDetails={setOrgDetails}
+          giftDetails={giftDetails}
+          setGiftDetails={setGiftDetails}
         />
         <Slider 
           type={RESERVED_EVENTS.errors}
-          orgDetails={orgDetails}
-          setOrgDetails={setOrgDetails}
+          giftDetails={giftDetails}
+          setGiftDetails={setGiftDetails}
         />
         <Input 
-          type={"Accepted Errors"}
-          orgDetails={orgDetails}
-          setOrgDetails={setOrgDetails}
+          type={EVENT_CONSTANTS.acceptedErrors}
+          giftDetails={giftDetails}
+          setGiftDetails={setGiftDetails}
         />
         <Slider 
           type={RESERVED_EVENTS.transactions}
-          orgDetails={orgDetails}
-          setOrgDetails={setOrgDetails}
+          giftDetails={giftDetails}
+          setGiftDetails={setGiftDetails}
         />
         <Input 
-          type={"Accepted Transactions"}
-          orgDetails={orgDetails}
-          setOrgDetails={setOrgDetails}
+          type={EVENT_CONSTANTS.acceptedTransactions}
+          giftDetails={giftDetails}
+          setGiftDetails={setGiftDetails}
         />
       </div>
       <div className="card--gifted-events">
-
+        <Input 
+          type={EVENT_CONSTANTS.giftedErrors}
+          giftDetails={giftDetails}
+          setGiftDetails={setGiftDetails}
+        />
+        <Input
+          type={EVENT_CONSTANTS.giftedTransactions}
+          giftDetails={giftDetails}
+          setGiftDetails={setGiftDetails}
+        />
       </div>
       <div className="card--computed-values">
 
