2eca2b9fd92302f77a7e2e1fee8246f073d670e7
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-08-11 21:03:06 -0400

    initialize event constants

---
+++ src/constants/EVENT_CONSTANTS.js
@@ -0,0 +1,20 @@
+const EVENT_CONSTANTS = {
+    acceptedErrors: {
+        title: "Accepted Errors",
+        key: "acceptedErrors",
+    },
+    acceptedTransactions: {
+        title: "Accepted Transactions",
+        key: "acceptedTransactions",
+    },
+    giftedErrors: {
+        title: "Gifted Errors",
+        key: "giftedErrors",
+    },
+    giftedTransactions: {
+        title: "Gifted Transactions",
+        key: "giftedTransactions"
+    },
+};
+
+module.exports = {EVENT_CONSTANTS};
