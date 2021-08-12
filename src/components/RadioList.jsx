b750a5ac3369a5f3f6bb46a98f7aaf50167978a6
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-08-11 21:03:47 -0400

    change orgDetails to giftDetails

--- src/components/RadioList.jsx
+++ src/components/RadioList.jsx
@@ -2,20 +2,20 @@ import '../App.css';
 
 export default function RadioList(props) {
 
-    const { type, orgDetails, setOrgDetails} = props;
+    const { type, giftDetails, setGiftDetails} = props;
     const { key, title, list } = type;
 
     function onChange(event) {
         const { target } = event;
 
-        setOrgDetails(prev => ({...prev, [key]: target.value}));
+        setGiftDetails(prev => ({...prev, [key]: target.value}));
     };
 
     const radioList = list.map(v => {
         return (
             <div className="radio-group" key={v}>
                 <label for={v}>{v}</label>
-                <input type="radio" name={title} value={v} id={v} onChange={onChange} checked={v === orgDetails[key] ? true : false}/>
+                <input type="radio" name={title} value={v} id={v} onChange={onChange} checked={v === giftDetails[key] ? true : false}/>
             </div>
         )
     }) 
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,20 +2,20 @@ import '../App.css';
 
 export default function Slider(props) {
 
-    const { type, orgDetails, setOrgDetails } = props;
+    const { type, giftDetails, setGiftDetails } = props;
     const { title, list, key } = type;
 
     function onChange(event) {
         const { target } = event;
 
-        setOrgDetails(prev => ({...prev, [key]: list[target.value]}));
+        setGiftDetails(prev => ({...prev, [key]: list[target.value]}));
     };
 
     return (
         <div className="slider-group">
             <label for={title} className="slider-label">{title}</label>
-            <input type="range" name={title} min={0} max={list.length - 1} step={1} onChange={onChange} value={list.indexOf(orgDetails[key])}/>
-            <label for={title} className="slider-value">{parseInt(orgDetails[key]) * 1000}</label>
+            <input type="range" name={title} min={0} max={list.length - 1} step={1} onChange={onChange} value={list.indexOf(giftDetails[key])}/>
+            <label for={title} className="slider-value">{parseInt(giftDetails[key]) * 1000}</label>
             <div className="slider-key"> 
                 <span>{list[0]}</span>
                 <span>{list[list.length-1]}</span>
