b45565ec254561806a0557273bfcec6d7eaa76ec
Nicholas Meisenheimer <souredoutlook@gmail.com>
2023-12-23 07:00:58 -0500

    add Sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from './App';
+
+Sentry.init({
+  dsn: "https://c580e4ff1e414676b3d0a90713092b86@o676634.ingest.sentry.io/5977689",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  denyUrls:[
+    /^http:\/\/localhost:/i
+  ],
+  debug: true,
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
